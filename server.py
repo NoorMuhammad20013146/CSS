eaf25e334fd7314deead0be4cc036820f9b0b5e7
Noor Muhammad <20013146@mydbs.ie>
2024-11-26 22:02:34 +0000

    added home route to serve the index.html

--- server.py
+++ server.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 key_queue = queue.Queue() # for key exchange
 message_queue = queue.Queue() #forencrypted  message exchange
 
+
+# Serve the index.html file
+@app.route('/')
+def home():
+    return render_template('index.html')
