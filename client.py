c5c36d5eecfa08f81341a0456f768827038cc5e9
Noor Muhammad <20013146@mydbs.ie>
2024-11-27 21:09:56 +0000

    added fetch key function to client side

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
 SERVER_URL = "http://127.0.0.1:5000"
-
+#send key function
 def send_key(public_key):
     try:
         response = requests.post(f"{SERVER_URL}/exchange_key", json={"key": public_key})
@@ -9,6 +9,12 @@ def send_key(public_key):
     except Exception as e:
         print("Error sending key:", str(e))
 
-        
+#fecth key function
+def fetch_key():
+    try:
+        response = requests.get(f"{SERVER_URL}/get_key")
+        print("Fetch Key Response:", response.json())
+    except Exception as e:
+        print("Error fetching key:", str(e))
+
 if __name__ == "__main__":
-    test_connection()
