c72a2526a9bc671fb1e1b0a23c23be6bb48d8b52
Noor Muhammad <20013146@mydbs.ie>
2024-11-27 21:04:18 +0000

    add send key function in client side

--- client.py
+++ client.py
@@ -2,12 +2,13 @@ import requests
 
 SERVER_URL = "http://127.0.0.1:5000"
 
-def test_connection():
+def send_key(public_key):
     try:
-        response = requests.get(SERVER_URL)
-        print("Server Response", response.text)
+        response = requests.post(f"{SERVER_URL}/exchange_key", json={"key": public_key})
+        print("Send Key Response:", response.json())
     except Exception as e:
-        print("Error to connecting the server")
+        print("Error sending key:", str(e))
 
+        
 if __name__ == "__main__":
     test_connection()
